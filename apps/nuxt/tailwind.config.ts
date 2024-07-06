fcc6acd0ede8e4a7a543fb856c16b25819d0b1ff
James Donnelly <4444684+JDIZM@users.noreply.github.com>
2024-07-06 19:54:22 +0100

    tailwind config

---
+++ apps/nuxt/tailwind.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from "tailwindcss"
+import sharedConfig from "tailwind-config"
+
+const config: Pick<Config, "content" | "presets"> = {
+  content: ["../../packages/ui/**/*.vue"],
+  presets: [sharedConfig]
+}
+
+export default config
