67bc4cdbcf0bfe25796bf1bc3c39ebca81140109
James Donnelly <4444684+JDIZM@users.noreply.github.com>
2025-10-05 11:45:27 +0100

    chore(nuxt): update dependencies and migrate to ESLint flat config
    - Migrate to ESLint flat config
    - Update package dependencies
    - Update nuxt.config.ts
    - Update tsconfig files
    - Update Vitest configuration
    - Update Tailwind config
    - Format existing code

--- apps/nuxt/nuxt.config.ts
+++ apps/nuxt/nuxt.config.ts
@@ -1,13 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt/config'
+
 export default defineNuxtConfig({
-  devtools: { enabled: true },
   modules: [
-    "@nuxtjs/tailwindcss",
-    "@pinia/nuxt", // https://pinia.vuejs.org/ssr/nuxt.html
-    "@nuxt/image" // https://image.nuxt.com
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt', // https://pinia.vuejs.org/ssr/nuxt.html
+    '@nuxt/image' // https://image.nuxt.com
   ],
+  devtools: { enabled: true },
   image: {
-    format: ["webp"],
+    format: ['webp'],
     quality: 80
   }
 })
